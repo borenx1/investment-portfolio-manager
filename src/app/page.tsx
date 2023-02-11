f0ba0be0dbe6a961c81e1b380b8ebc9e1815ed82
Boren Xue <53597966+borenx1@users.noreply.github.com>
2023-02-12 01:31:59 +1300

    Pass Vercel deployment checks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,4 +2,6 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   redirect('/dashboard');
+  // Should not reach this point.
+  return <h1>Investment Portfolio Manager</h1>;
 }
